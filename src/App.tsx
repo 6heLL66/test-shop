a7137d3b89550a3d0cce206724d3a3e94996180d
getlost <sseniknik@gmail.com>
2023-12-06 07:40:37 +0300

    fix: uncomment sw

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,16 @@ const queryClient = new QueryClient({
   },
 })
 
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker
-//     .register('/service-worker.js')
-//     .then(function (registration) {
-//       console.log('Service Worker registered with scope:', registration.scope)
-//     })
-//     .catch(function (error) {
-//       console.error('Error while Service Worker register:', error)
-//     })
-// }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/service-worker.js')
+    .then(function (registration) {
+      console.log('Service Worker registered with scope:', registration.scope)
+    })
+    .catch(function (error) {
+      console.error('Error while Service Worker register:', error)
+    })
+}
 
 export const App = () => {
   return (
